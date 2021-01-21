7cace6e47388a7782277a0cd99abf378f4fb763c
Nagy Salem <me@muhnagy.com>
2021-01-21 23:21:29 +0200

    Added Pipedrive create person and create activity functions

---
+++ models/pipedrive.go
@@ -0,0 +1,13 @@
+package models
+
+type PipedriveCreatePersonResponse struct {
+	Data struct {
+		ID int `json:"id"`
+	} `json:"data"`
+	PipedriveResponseStatus
+}
+
+type PipedriveResponseStatus struct {
+	Error   string `json:"error"`
+	Success bool   `json:"success"`
+}
---
+++ services/pipedrive/pipedrive.go
@@ -0,0 +1,76 @@
+package pipedrive
+
+import (
+	"bytes"
+	"encoding/json"
+	"fmt"
+	"github.com/muhammednagy/pipedirve-challenge/models"
+	log "github.com/sirupsen/logrus"
+	"io/ioutil"
+	"net/http"
+)
+
+const pipedriveBase = "https://api.pipedrive.com/v1/"
+
+func makeRequest(token, path string, body []byte) ([]byte, error) {
+	res, err := http.Post(pipedriveBase+path+"?api_token="+token, "application/json", bytes.NewBuffer(body))
+	if err != nil {
+		return nil, fmt.Errorf("failed to send request error: %s", err)
+	}
+	body, err = ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, fmt.Errorf("failed to read request response error: %s", err)
+	}
+	return body, nil
+}
+
+func CreatePerson(username, token string) int {
+	var createPersonResponse models.PipedriveCreatePersonResponse
+	parameters := map[string]string{
+		"name": username,
+	}
+	parametersJSON, _ := json.Marshal(parameters)
+
+	body, err := makeRequest(token, "persons", parametersJSON)
+	if err != nil {
+		log.Error("Failed to create person in Pipedrive error: ", err)
+		return 0
+	}
+	err = json.Unmarshal(body, &createPersonResponse)
+	if err != nil {
+		log.Error("Failed to create person in Pipedrive error: ", err)
+		return 0
+	}
+	if !createPersonResponse.Success {
+		log.Error("Failed to create person in Pipedrive error: ", createPersonResponse.Error)
+		return 0
+	}
+	return createPersonResponse.Data.ID
+}
+
+func CreateActivity(subject, note, token string, personID int) error {
+	var status models.PipedriveResponseStatus
+	parameters := struct {
+		Subject  string `json:"subject"`
+		Note     string `json:"note"`
+		PersonId int    `json:"person_id"`
+	}{
+		subject,
+		note,
+		personID,
+	}
+	parametersJSON, _ := json.Marshal(parameters)
+
+	body, err := makeRequest(token, "activities", parametersJSON)
+	if err != nil {
+		return fmt.Errorf("failed to create activity in Pipedrive error: %s", err)
+	}
+	err = json.Unmarshal(body, &status)
+	if err != nil {
+		return fmt.Errorf("failed to create activity in Pipedrive error: %s", err)
+	}
+	if !status.Success {
+		return fmt.Errorf("failed to create activity in Pipedrive error: %s", status.Error)
+	}
+	return fmt.Errorf(status.Error)
+}
