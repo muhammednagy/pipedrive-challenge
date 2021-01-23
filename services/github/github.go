3536c624f8a69c5458e7cccc75d557b114ec745a
Nagy Salem <me@muhnagy.com>
2021-01-23 05:10:45 +0200

    Finalized Github GetGists function

--- services/github/github.go
+++ services/github/github.go
@@ -2,68 +2,61 @@ package github
 
 import (
 	"context"
+	"fmt"
 	"github.com/google/go-github/v33/github"
-	"github.com/labstack/gommon/log"
-	"github.com/muhammednagy/pipedirve-challenge/db"
 	"github.com/muhammednagy/pipedirve-challenge/models"
 	"golang.org/x/oauth2"
-	"gorm.io/gorm"
 	"net/http"
+	"time"
 )
 
 // Get Gists
 // Will get all gists by a user in one requests if they are less than 100 or more than one request if they are more
 // if you provide githubToken it will be able to do more requests before it gets rate limited
-func GetGists(config models.Config, dbConnection *gorm.DB, username, githubToken string) []*github.Gist {
-	var person models.Person
+func GetGists(config models.Config, lastVisit *time.Time, username string) ([]*github.Gist, error) {
 	tc := &http.Client{}
 	ctx := context.Background()
 	if config.GithubToken != "" {
 		ts := oauth2.StaticTokenSource(
-			&oauth2.Token{AccessToken: githubToken},
+			&oauth2.Token{AccessToken: config.GithubToken},
 		)
 		tc = oauth2.NewClient(ctx, ts)
 	}
 	client := github.NewClient(tc)
 
-	people := db.GetPeople(dbConnection, username)
-	if len(people) == 0 {
-		_ = db.SavePerson(dbConnection, username)
-	} else {
-		person = people[0]
+	gistListOptions := github.GistListOptions{}
+	if lastVisit != nil {
+		gistListOptions.Since = *lastVisit
 	}
 
-	gists, response, err := client.Gists.List(ctx, username, &github.GistListOptions{
-		Since: person.LastVisit,
-	})
+	gists, response, err := client.Gists.List(ctx, username, &gistListOptions)
 	if response == nil || err != nil {
-		log.Errorf("error occurred fetching user gists", err)
-		return nil
+		return nil, fmt.Errorf("error occurred fetching user gists: %s", err)
 	}
 	if response.StatusCode != 200 {
-		log.Errorf("error occurred fetching user gists")
-		return nil
+		return nil, fmt.Errorf("error occurred fetching user gists: %s", response.Status)
 	}
 
 	if response.NextPage != 0 {
 		pagesCount := response.LastPage - 1
 		for i := 2; i < pagesCount; i++ {
-			newGists, response, err := client.Gists.List(ctx, username, &github.GistListOptions{
-				Since: person.LastVisit,
+			gistListOptions := github.GistListOptions{
 				ListOptions: github.ListOptions{
 					Page: i,
 				},
-			})
+			}
+			if lastVisit != nil {
+				gistListOptions.Since = *lastVisit
+			}
+			newGists, response, err := client.Gists.List(ctx, username, &gistListOptions)
 			if response == nil || err != nil {
-				log.Errorf("error occurred fetching user gists", err)
-				return nil
+				return nil, fmt.Errorf("error occurred fetching user gists: %s", err)
 			}
 			if response.StatusCode != 200 {
-				log.Errorf("error occurred fetching user gists")
-				return nil
+				return nil, fmt.Errorf("error occurred fetching user gists: %s", response.Status)
 			}
 			gists = append(gists, newGists...)
 		}
 	}
-	return gists
+	return gists, nil
 }
